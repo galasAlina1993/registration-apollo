beebdd1f5255ac1feb39ee6ddb6a6e2c49c83386
galas.alina1993 <galas.alina1993@gmail.com>
2020-04-08 17:17:14 +0300

    control for the file uploading

--- client/src/components/registrationForm.js
+++ client/src/components/registrationForm.js
@@ -4,9 +4,9 @@ import NumberFormat from 'react-number-format';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 import { Formik, Field, Form } from 'formik';
-import { TextField } from 'formik-material-ui';
-import * as Yup from 'yup';
+import { TextField, SimpleFileUpload } from 'formik-material-ui';
 import { GET_USERS } from './userList';
+import * as Yup from 'yup';
 
 const CREATE_USER = gql`
   mutation createUser(
@@ -50,92 +50,109 @@ export const RegistrationForm = () => {
     });
 
     return (
-        <div id='registrationForm'>
-            <h3>Registration page</h3>
-            <Formik
-                initialValues={{
-                    name: '',
-                    email: '',
-                    phone: '',
-                    address: '',
-                    zipCode: ''
+            <div id='registrationForm'>
+                <h3>Registration page</h3>
+                <Formik
+                    initialValues={{
+                        name: '',
+                        email: '',
+                        phone: '',
+                        address: '',
+                        zipCode: ''
 
-                }}
-                validationSchema={Yup.object().shape({
-                    name: Yup.string().required('First Name is required'),
-                    email: Yup.string()
-                        .email('Email is invalid')
-                        .required('Email is required'),
-                    phone: Yup.string()
-                        .required('Phone is required')
-                        .min(12, 'Incorrect Phone Number'),
-                    address: Yup.string()
-                        .required('Address is required'),
-                    zipCode: Yup.string()
-                        .required('Zip Code is required')
+                    }}
+                    validationSchema={Yup.object().shape({
+                        name: Yup.string().required('First Name is required'),
+                        email: Yup.string()
+                            .email('Email is invalid')
+                            .required('Email is required'),
+                        phone: Yup.string()
+                            .required('Phone is required')
+                            .min(12, 'Incorrect Phone Number'),
+                        address: Yup.string()
+                            .required('Address is required'),
+                        zipCode: Yup.string()
+                            .required('Zip Code is required')
 
-                })}
-                onSubmit={(fields, { resetForm }) => {
-                    createUser({ variables: { ...fields } });
-                    resetForm();
-                }}
+                    })}
+                    onSubmit={(fields, { resetForm }) => {
+                        createUser({ variables: { ...fields } });
+                        resetForm();
+                    }}
 
-                render={({ errors, status, touched }) => (
-                    <Form>
-                        <Field
-                            label='Name'
-                            name='name'
-                            type='text'
-                            component={TextField}
-                            margin='none'
-                            variant='outlined'
-                            fullWidth
-                        />
-                        <Field
-                            label='Email'
-                            name='email'
-                            type='text'
-                            component={TextField}
-                            margin='normal'
-                            variant='outlined'
-                            fullWidth
-                        />
-                        <Field
-                            label='Phone'
-                            name='phone'
-                            type='text'
-                            component={CustomPhoneField}
-                            margin='normal'
-                            variant='outlined'
-                            fullWidth
-                        />
-                        <Field
-                            label='Address'
-                            name='address'
-                            type='text'
-                            component={TextField}
-                            margin='normal'
-                            variant='outlined'
-                            fullWidth
-                        />
-                        <Field
-                            label='Zip Code'
-                            name='zipCode'
-                            type='text'
-                            component={TextField}
-                            margin='normal'
-                            variant='outlined'
-                            fullWidth
-                        />
-                        <Button type='submit' variant='outlined' color='primary'>
-                            Register
+                    render={({ errors, status, touched }) => (
+                        <Form>
+                            <Field
+                                label='Name'
+                                name='name'
+                                type='text'
+                                component={TextField}
+                                margin='none'
+                                variant='outlined'
+                                fullWidth
+                            />
+                            <Field
+                                label='Email'
+                                name='email'
+                                type='text'
+                                component={TextField}
+                                margin='normal'
+                                variant='outlined'
+                                fullWidth
+                            />
+                            <Field
+                                label='Phone'
+                                name='phone'
+                                type='text'
+                                component={CustomPhoneField}
+                                margin='normal'
+                                variant='outlined'
+                                fullWidth
+                            />
+                            <Field
+                                label='Address'
+                                name='address'
+                                type='text'
+                                component={TextField}
+                                margin='normal'
+                                variant='outlined'
+                                fullWidth
+                            />
+                            <Field
+                                label='Zip Code'
+                                name='zipCode'
+                                type='text'
+                                component={TextField}
+                                margin='normal'
+                                variant='outlined'
+                                fullWidth
+                            />
+                            <Field
+                                className='file-upload'
+                                name='file-upload'
+                                type='file'
+                                component={SimpleFileUpload}
+                                margin='normal'
+                                variant='outlined'
+                                fullWidth
+                            />
+                            <Field
+                                name='files'
+                                type='hidden'
+                                margin='normal'
+                                variant='outlined'
+                                fullWidth
+                            />
+
+                            <Button type='submit' variant='outlined' color='primary'>
+                                Register
                 </Button>{' '}
-                        <Button type='reset' variant='outlined' color='secondary'>
-                            Reset
+                            <Button type='reset' variant='outlined' color='secondary'>
+                                Reset
                 </Button>
-                    </Form>
-                )}
-            ></Formik>
-        </div>
+                        </Form>
+                    )}
+                ></Formik>
+            </div>
     );
 }
